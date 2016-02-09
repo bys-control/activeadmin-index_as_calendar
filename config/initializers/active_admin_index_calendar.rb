6ba0f089d949bfa0902758e80619876e606b1e9b
Guillermo Bisheimer <gbisheimer@bys-control.com.ar>
2016-02-09 20:18:53 +0000

    Add helper functions for index initialization

--- config/initializers/active_admin_index_calendar.rb
+++ config/initializers/active_admin_index_calendar.rb
@@ -4,24 +4,12 @@ module ActiveAdmin
 
       def build(page_presenter, collection)
         add_class "calendar"
-        context = {:page_presenter => page_presenter, :collection => collection}
+        context = {:page_presenter => page_presenter, :collection => collection, :fullCalendarOptions => nil}
         events = instance_exec(context, &page_presenter.block) unless page_presenter.block.blank?
-
-        # Builds default events array for collection if page_presenter.block returns no data
-        if events.blank?
-          events = collection.map do |item|
-            {
-              id: item.id,
-              title: item.to_s,
-              start: item.created_at.blank? ? Date.today.to_s : item.created_at,
-              url: "#{auto_url_for(item)}"
-            }
-          end
-        end
 
         # Render fullCalendar
         panel "Calendar", id: "calendar" do
-          render :partial => "calendar", locals: {events: events, options: context[:fullCalendar_options].to_json.html_safe}
+          render :partial => "calendar", locals: {events: events, options: context[:fullCalendarOptions].to_json.html_safe}
         end
       end
 
--- lib/index_as_calendar/dsl.rb
+++ lib/index_as_calendar/dsl.rb
@@ -1,5 +1,70 @@
 module ActiveAdminIndexAsCalendar
   module DSL
 
+    #
+    # Initializes activeadmin index as calendar
+    #
+    def index_as_calendar( options={}, &block )
+      default_options = {
+        :ajax => nil,
+        :model => nil,   # Needed only for AJAX
+        :includes => nil, # Eager loading of related models
+        :start_field => :created_at, # Default
+        :end_field => nil,
+        :block => block
+      }
+      options = default_options.deep_merge(options)
+
+      # Defines controller for event_mapping model items to events
+      controller do
+        def event_mapping( items, options )
+          events = items.map do |item|
+            if !options[:block].blank?
+              instance_exec(item, &options[:block])
+            else
+              {
+                :id => item.id,
+                :title => item.to_s,
+                :start => (options[:start_field].blank? or item.send(options[:start_field]).blank?) ? Date.today.to_s : item.send(options[:start_field])
+              }
+            end
+          end
+        end
+      end
+
+      # Setup AJAX
+      if options[:ajax]
+
+        # Setup fullCalendar to use AJAX calls to retrieve event data
+        index as: :calendar do |context|
+          events = {
+            url: "#{collection_path()}/index_as_events.json",
+            type: 'GET',
+            data: params
+          }
+        end
+
+        # Defines collection_action to get events data
+        collection_action :index_as_events, :method => :get do
+          items = options[:model] || end_of_association_chain
+          items = items.send(params[:scope]) if params[:scope].present?
+          items = items.includes(options[:includes]) unless options[:includes].blank?
+          items = items.where(options[:start_field] => params[:start].to_date...params[:end].to_date).search(params[:q]).result
+
+          events = event_mapping(items, options)
+
+          respond_to do |format|
+            format.json { render :json => events }
+          end
+        end
+
+      # Return events to be used during partial render
+      else
+        index as: :calendar do |context|
+          events = self.controller.event_mapping(context[:collection], options)
+        end
+      end
+    end
+
   end
 end
